d6705d6bed75b83d14d1f1558563f57c0a1961cb
Gustavo de França <gustavonota20@hotmail.com>
2024-04-08 16:09:27 -0300

    Alterando visibilidade de alguns arquivos

--- src/main/java/com/spring/apicotacao/service/TranslationService.java
+++ src/main/java/com/spring/apicotacao/service/TranslationService.java
@@ -2,12 +2,15 @@ package com.spring.apicotacao.service;
 
 import com.spring.apicotacao.model.Translation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TranslationService {
     @Autowired
     private TranslationAPI languageTranslationClient;
+    @Value("${translation.api.key}")
+    private String apiKey;
 
     public String translateText(String text, String targetLanguage) {
         String apiKey = "f6RsaoycwfcjKwPN83lcNYetXCdKINow";
